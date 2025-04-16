3d9497ebf072418e135bd438af505dbce36d4aca
延枚 <yanmei.hbw@alibaba-inc.com>
2025-04-16 15:19:23 +0800

    去掉计算tool

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"context"
 	"devops.aliyun.com/mcp-yunxiao/tools/codeup"
 	"devops.aliyun.com/mcp-yunxiao/tools/projex"
-	"errors"
 	"fmt"
 
-	"github.com/mark3labs/mcp-go/mcp"
 	"github.com/mark3labs/mcp-go/server"
 )
 
@@ -19,48 +16,6 @@ func main() {
 		server.WithResourceCapabilities(true, true),
 		server.WithLogging(),
 	)
-
-	// Add a calculator tool
-	calculatorTool := mcp.NewTool("calculate",
-		mcp.WithDescription("Perform basic arithmetic tools"),
-		mcp.WithString("operation",
-			mcp.Required(),
-			mcp.Description("The operation to perform (add, subtract, multiply, divide)"),
-			mcp.Enum("add", "subtract", "multiply", "divide"),
-		),
-		mcp.WithNumber("x",
-			mcp.Required(),
-			mcp.Description("First number"),
-		),
-		mcp.WithNumber("y",
-			mcp.Required(),
-			mcp.Description("Second number"),
-		),
-	)
-
-	// Add the calculator handler
-	s.AddTool(calculatorTool, func(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
-		op := request.Params.Arguments["operation"].(string)
-		x := request.Params.Arguments["x"].(float64)
-		y := request.Params.Arguments["y"].(float64)
-
-		var result float64
-		switch op {
-		case "add":
-			result = x + y
-		case "subtract":
-			result = x - y
-		case "multiply":
-			result = x * y
-		case "divide":
-			if y == 0 {
-				return nil, errors.New("Cannot divide by zero")
-			}
-			result = x / y
-		}
-
-		return mcp.NewToolResultText(fmt.Sprintf("%.2f", result)), nil
-	})
 
 	s.AddTool(codeup.CreateChangeRequestTool, codeup.CreateChangeRequestFunc)
 

7fd8def5930c907100ad48ceae21d37c245e2b18
延枚 <yanmei.hbw@alibaba-inc.com>
2025-06-10 14:46:37 +0800

    添加获取部门成员的tool

---
+++ operations/organization/members.ts
@@ -0,0 +1,33 @@
+import { oapiClient } from '../client';
+import { z } from 'zod';
+import {OrganizationMembersSchema, MemberInfoSchema, OrganizationMembers} from '../../common/types';
+import {buildUrl, yunxiaoRequest} from "../../common/utils.js";
+
+/**
+ * 查询组织成员列表
+ * @param organizationId 组织ID
+ * @param page 当前页，默认1
+ * @param perPage 每页数据条数，默认100
+ * @returns 组织成员列表
+ */
+export const getOrganizationMembersFunc = async (
+  organizationId: string,
+  page: number = 1,
+  perPage: number = 100
+): Promise<OrganizationMembers> => {
+
+  const url = `/oapi/v1/platform/organizations/${organizationId}/members`;
+
+  const params = {
+    page: page,
+    perPage: perPage
+  };
+  const urlWithParams = buildUrl(url, params);
+
+  const response = await yunxiaoRequest(urlWithParams, {
+    method: "GET",
+  });
+
+  // 验证响应数据结构
+  return OrganizationMembersSchema.parse(response);
+};
